97c03f6c3b6c28b243381e63ca156ff0d3e93d48
mkelly1 <mkelly@his.com>
2019-09-04 11:55:35 -0400

    Math Two commit 1

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args){
+        int myNumber = 2;
+        double myOtherNumber = 1.7938;
+        System.out.println("My number = "+myNumber+" and my other number = "+myOtherNumber);
+    }
+}
